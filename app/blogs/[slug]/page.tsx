55d3542ddb28be730dd66b11f75321ad8d58b3d4
Ajay2903 <78751003+Ajay2903@users.noreply.github.com>
2026-03-22 20:49:08 +0530

    Updated the blog page

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -16,9 +16,16 @@ export default async function BlogPost({ params }: BlogParams) {
   const { content, data } = matter(fileContent)
 
   return (
-    <article className="container mx-auto py-12 prose">
-      <h1 className="text-4xl font-bold">{data.title}</h1>
+    <article className="flex flex-col gap-4 w-full max-w-3xl mx-auto py-12 px-4">
+      <h1 className="text-3xl font-bold md:text-4xl">{data.title}</h1>
+      <div className="flex flex-row">
+      <p className="text-sm text-gray-500">by Ajay Tibrewal</p>
+      <div className="flex-1"></div>
       <p className="text-sm text-gray-500">{data.date}</p>
+
+      </div>
+      <img className="w-full h-64  object-cover" src={data.src} alt="img" />
+      
       <MDXRenderer source={content} />
     </article>
   )
